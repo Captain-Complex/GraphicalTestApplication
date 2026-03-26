0034b987008e786dcb1e00a36e1bb401b20f8872
AIE\s251987 <221801567+Captain-Complex@users.noreply.github.com>
2026-03-26 16:58:24 +1100

    going through basic matrix translation and rotation

--- GraphicalTestApplcation/GraphicalTestApplcation/main.cpp
+++ GraphicalTestApplcation/GraphicalTestApplcation/main.cpp
@@ -1,19 +1,43 @@
 #include <raylib.h>
 #include <raymath.h>
 
+#include <cmath>
+#include <iostream>
+#include <numbers>
 #include<string>
 
 int main()
 {
+	int screenWidth = 1280;
+	int screenHeight = 720;
 	SetTraceLogLevel(LOG_ERROR);
 	InitWindow(1280, 720, "Demo");
 	SetTargetFPS(60);
 
 	Matrix vehicle = MatrixIdentity();
-	vehicle = MatrixTranslate(200, 200, 0);
+	Matrix vehicleRotation = MatrixRotateZ(45 * (PI / 180));
+	Matrix vehicleTranslation = MatrixTranslate(screenWidth / 2, screenHeight / 2, 0);
+	vehicle = MatrixMultiply(
+	vehicleRotation,
+	vehicleTranslation);
+
+	Vector2 vehiclePoints[]
+	{
+		{-10.f, -15.f},
+		{-10.f, 15.f},
+		{10.f, 15.f},
+		{10.f, -15.f},
+	};
+	const int pointSize = sizeof(vehiclePoints) / sizeof(vehiclePoints[0]);
+	for (Vector2& point : vehiclePoints)
+	{
+		point = Vector2Transform(point, vehicle);
+	}
 
 	Vector2 lineStart{ 30, 30 };
 	Vector2 lineEnd{ 0, 0 };
+	//lineEnd = Vector2Transform(lineEnd, vehicle);
+	//lineEnd = (lineEnd.x, lineEnd.y, 0, 1)
 
 	while (!WindowShouldClose())
 	{
@@ -50,7 +74,17 @@ int main()
 		angleDiff *= 180 / 3.14159f;
 		std::string speedText = std::to_string(angleDiff);
 
-        DrawLineEx(lineStart, lineEnd, 16.f, WHITE);
+		for (int i = 0; i < pointSize; ++i)
+		{
+			DrawLineEx(
+				vehiclePoints[i],
+				vehiclePoints[(i + 1) % pointSize],
+				4.f,
+				RED
+			);
+		}
+
+        //DrawLineEx(lineStart, lineEnd, 16.f, WHITE);
 		DrawText(speedText.c_str(), 32, 32, 32, BLACK);
 
 
